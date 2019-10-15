e441f8cae3d9e09aab9a2f084c8df26d8b76ddc9
Damsss <damien.cuiec-aime@gadz.org>
2019-10-15 15:24:22 -0400

    Ajout GameOver quand chute et respawn

---
+++ Assets/GameManager.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    bool gameHasEnded = false;
+    public GameObject cam;
+    public GameObject gameOverUI;
+
+    void Start()
+    {
+        FindObjectOfType<KillPlayer>().OnPlayerDeath += EndGame;
+    }
+    void EndGame()
+    {
+        if (!gameHasEnded)
+        {
+            gameHasEnded = true;
+            print("GameOver");
+            Destroy(cam);
+            gameOverUI.SetActive(true);
+
+
+
+        }
+        
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && gameHasEnded)
+        {
+            Restart();
+        }
+    }
+
+
+    void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
---
+++ Assets/Script/KillPlayer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KillPlayer : MonoBehaviour
+{
+    
+    public float terrainLimiteZ = -5f;
+    public event System.Action OnPlayerDeath;
+
+    
+    private void Update()
+    {
+        if (transform.position.y < terrainLimiteZ)
+        {
+            OnPlayerDeath?.Invoke();
+
+        }
+        if (transform.position.y < terrainLimiteZ-10)
+        {
+            Destroy(gameObject);
+        }
+    }
+    
+}
