20b336a67586ced454d304b8b52b837498860932
Damien <damien.cuiec-aime@pulets.ca>
2019-10-24 19:44:59 -0400

    Start Menu et changement UI

---
+++ Assets/GameMenu.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameMenu : MonoBehaviour
+{
+    public void RestartLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void ReturnToMenu()
+    {
+        SceneManager.LoadScene("Start Menu");
+    }
+}
